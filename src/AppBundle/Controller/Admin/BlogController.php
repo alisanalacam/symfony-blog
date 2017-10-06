9b2cb87de8b395e4996e6ae52d8f9080904158c4
Alişan ALAÇAM <alisan@digitalcake.com.tr>
2017-10-06 11:56:36 +0300

    adminden yeni makale ekleme controllerı yapıldı

--- src/AppBundle/Controller/Admin/BlogController.php
+++ src/AppBundle/Controller/Admin/BlogController.php
@@ -27,5 +27,41 @@ class BlogController extends Controller
         $posts = $entityManager->getRepository('AppBundle:Blog\Post')->findAll();
         return $this->render('admin/blog/index.html.twig', array('posts' => $posts));
     }
+    /**
+     * Yeni Makale oluşturur
+     *
+     * @Route("/new", name="admin_post_new")
+     * @Method({"GET", "POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+        $post->setAuthor($this->getUser()->getId());
+
+        $form = $this->createForm('AppBundle\Form\PostType', $post)
+            ->add('saveAndCreateNew', 'Symfony\Component\Form\Extension\Core\Type\SubmitType')
+        ;
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($this->get('slugger')->slugify($post->getTitle()));
+            $entityManager = $this->getDoctrine()->getManager();
+            var_dump($post->getAuthor());
+            $entityManager->persist($post);
+            $entityManager->flush();
+            $this->addFlash('success', 'Makale başarıyla oluşturuldu');
+            if ($form->get('saveAndCreateNew')->isClicked()) {
+                return $this->redirectToRoute('admin_post_new');
+            }
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/blog/new.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }
 
 }
